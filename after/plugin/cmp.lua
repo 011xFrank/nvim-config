153d5a689a5cb9e2be3e4ec5f2ae0405ff44b6f2
011xFrank <francis.l.muhanji@gmail.com>
2024-03-19 22:41:11 +0300

    neovim config

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -9,6 +9,7 @@ cmp.setup({
     },
     window = {
         completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
     },
     snippet = {
         expand = function(args)
